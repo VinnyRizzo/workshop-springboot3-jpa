9b52e1be95fe86a73e0bcb82f65a3e14df78e8e4
Vinny Rizzo <viniciusbragarizzo@gmail.com>
2025-07-19 14:06:51 -0300

    Exception handling - delete

--- src/main/java/com/exemple/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/exemple/course/services/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,11 @@
 package com.exemple.course.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
 	
-	public ResourceNotFoundException(Object id) {
-		super("Resource not found. Id" + id);
-	}
+	   private static final long serialVersionUID = 1L;
+	
+	   public ResourceNotFoundException(Object id) {
+		       super("Resource not found. Id" + id);
+	   }
 
 }
